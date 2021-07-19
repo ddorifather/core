4ae14b329e2e1a4883bc3cd482fd179cfb064829
jiho <hanjiho@kakao.com>
2021-07-20 00:27:31 +0900

    2021년 7월19일

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -13,8 +13,11 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+        //MemberService memberService = new MemberServiceImpl(null);
+        //OrderService orderService = new OrderServiceImpl(null, null);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
